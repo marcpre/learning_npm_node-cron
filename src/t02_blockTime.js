84b9c0d5501b471115265a250feec2cfb42e486d
marcpre <marcus.presich@gmail.com>
2019-09-01 20:18:38 +0200

    fixing blocktime and run in parallel

--- src/t02_blockTime.js
+++ src/t02_blockTime.js
@@ -4,6 +4,7 @@ console.log('Before job instantiation');
 const job3 = new CronJob(
   '*/2 * * * * *', async () => {
     if (job3.taskRunning) {
+      console.log("wait for it...");
       return
     }
 
@@ -12,12 +13,12 @@ const job3 = new CronJob(
       await setTimeout(() => {
         const d = new Date();
         console.log('JOB 3 - ', d);
-        job3.taskRunning = true
+        job3.taskRunning = false
       }, 6000);
     } catch (err) {
       console.log(err);
     }
-    job3.taskRunning = false
+    job3.taskRunning = true
   }
 )
 console.log('After job instantiation');
